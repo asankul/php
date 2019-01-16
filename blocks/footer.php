eb98d6a13defa5e1bcabbfb23a4c031ffa8b4509
Asankul <asankul1993@gmail.com>
2019-01-16 18:01:35 +0600

    Merge

---
+++ blocks/footer.php
@@ -0,0 +1,4 @@
+<footer class="my-5 pt-5 text-muted text-center text-small">
+    <p class="mb-1">Asankul Sadykov - php intern</p>
+ 
+</footer>
---
+++ index.php
@@ -0,0 +1,67 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Jekyll v3.8.5">
+    <title>Создание статей</title>
+
+    <!-- Bootstrap core CSS -->
+<link href="/docs/4.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+    <!-- Custom styles for this template -->
+    <link href="form-validation.css" rel="stylesheet">
+  </head>
+  <body class="bg-light">
+    <div class="container">
+  <?php require 'blocks/header.php'; ?>
+
+  <div class="row">
+    <?php require 'blocks/tags.php'; ?>
+    <div class="col-md-8 order-md-1">
+      <h4 class="mb-3">Добавление новой статьи</h4>
+      <form class="needs-validation" novalidate>
+       
+
+        <div class="mb-3">
+          <label for="title">Введите тему статьи</label>
+          <div class="input-group">
+            <input type="text" class="form-control" id="title" name='title' placeholder="Тема статьи" required>
+          </div>
+        </div>
+
+        <div class="mb-3">
+          <label for="intro">Введите текст статьи</label>
+          <div class="input-group">
+            <textarea class="form-control" id="intro" name='intro' placeholder="Тема статьи" required></textarea>
+          </div>
+        </div>
+        <hr class="mb-4">
+        <button class="btn btn-primary btn-lg btn-block" type="submit">Continue to checkout</button>
+      </form>
+    </div>
+  </div>
+
+  <?php require 'blocks/footer.php'; ?>
+</div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script>window.jQuery || document.write('<script src="/docs/4.2/assets/js/vendor/jquery-slim.min.js"><\/script>')</script><script src="/docs/4.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-zDnhMsjVZfS3hiP7oCBRmfjkQC4fzxVxFhBx8Hkz2aZX8gEvA/jsP3eXRCvzTofP" crossorigin="anonymous"></script>
+        <script src="form-validation.js"></script></body>
+</html>

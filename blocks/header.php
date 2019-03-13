b378449c28b94060e870f3a401ccdbf8326217cd
asankul <34814424+asankul@users.noreply.github.com>
2019-03-14 04:48:37 +0600

    Update header.php

--- blocks/header.php
+++ blocks/header.php
@@ -1,5 +1,5 @@
 <div class="py-5 text-center">
     <img class="d-block mx-auto mb-4" src="/docs/4.2/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
     <h2>Тестовое задание</h2>
-    <p class="lead">Для SocialShopWave</p>
+    <p class="lead"></p>
   </div>
